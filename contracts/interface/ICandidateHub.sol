554602c2d83c879739c337208b337d378edf7b87
gilad <giladha@gmail.com>
2023-11-27 09:39:56 +0200

    tmp delete

--- contracts/interface/ICandidateHub.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.4; 
-
-interface ICandidateHub {
-  function canDelegate(address agent) external view returns(bool);
-  function getRoundTag() external view returns(uint256);
-}
